813244d252b0de56536222d584243d959523009d
poxyx <orangutancoder@gmail.com>
2020-04-12 00:26:50 +0800

    add option value

--- src/views/RestaurantRegister.vue
+++ src/views/RestaurantRegister.vue
@@ -15,6 +15,7 @@
                 @input="$v.name.$touch()"
                 @blur="$v.name.$touch()"
               ></v-text-field>
+
               <v-text-field
                 v-model="email"
                 :error-messages="emailErrors"
@@ -44,6 +45,23 @@
                 @blur="$v.repeatPassword.$touch()"
               ></v-text-field>
 
+              <v-row>
+                <v-col cols="4">
+                  <v-text-field v-model="countryCode" label="Country Code" disabled></v-text-field>
+                </v-col>
+                <v-col cols="8">
+                  <v-text-field
+                    v-model="phone"
+                    :error-messages="phoneErrors"
+                    label="Phone Number"
+                    type="tel"
+                    required
+                    @input="$v.phone.$touch()"
+                    @blur="$v.phone.$touch()"
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+
               <v-textarea v-model="description" clearable clear-icon="cancel" label="Description"></v-textarea>
 
               <v-select
@@ -70,10 +88,60 @@
                 v-model="orderTypeSelect"
                 :items="orderType"
                 :error-messages="orderTypeSelectErrors"
-                label="Type"
+                label="Order Type"
                 required
                 @change="$v.orderTypeSelect.$touch()"
                 @blur="$v.orderTypeSelect.$touch()"
+              ></v-select>
+
+              <v-select
+                v-model="collectionTimeSelect"
+                :items="collectionTime"
+                :error-messages="collectionTimeSelectErrors"
+                label="Collection Time"
+                required
+                @change="$v.collectionTimeSelect.$touch()"
+                @blur="$v.collectionTimeSelect.$touch()"
+              ></v-select>
+
+              <v-text-field
+                v-model="deliveryFee"
+                :error-messages="deliveryFeeErrors"
+                label="Delivery Fee"
+                type="number"
+                required
+                @input="$v.deliveryFee.$touch()"
+                @blur="$v.deliveryFee.$touch()"
+              ></v-text-field>
+
+              <v-text-field
+                v-model="minAmountForDelivery"
+                :error-messages="minAmountForDeliveryErrors"
+                label="Minimum Amount For Delivery"
+                type="number"
+                required
+                @input="$v.minAmountForDelivery.$touch()"
+                @blur="$v.minAmountForDelivery.$touch()"
+              ></v-text-field>
+
+              <v-select
+                v-model="minDeliveryTimeSelect"
+                :items="minDeliveryTime"
+                :error-messages="minDeliveryTimeSelectErrors"
+                label="Minimum Delivery Time"
+                required
+                @change="$v.minDeliveryTimeSelect.$touch()"
+                @blur="$v.minDeliveryTimeSelect.$touch()"
+              ></v-select>
+
+              <v-select
+                v-model="maxDeliveryTimeSelect"
+                :items="maxDeliveryTime"
+                :error-messages="maxDeliveryTimeSelectErrors"
+                label="Maximum Delivery Time"
+                required
+                @change="$v.maxDeliveryTimeSelect.$touch()"
+                @blur="$v.maxDeliveryTimeSelect.$touch()"
               ></v-select>
 
               <v-btn class="mt-5 mr-4" @click="submit">submit</v-btn>
@@ -107,9 +175,15 @@ export default {
   validations: {
     name: { required, maxLength: maxLength(10) },
     email: { required, email },
+    phone: { required },
     cuisineSelect: { required },
     typeSelect: { required },
+    deliveryFee: { required },
+    minAmountForDelivery: { required },
+    maxDeliveryTimeSelect: { required },
+    minDeliveryTimeSelect: { required },
     orderTypeSelect: { required },
+    collectionTimeSelect: { required },
     password: {
       required,
       minLength: minLength(6)
@@ -121,14 +195,118 @@ export default {
   },
 
   data: () => ({
+    countryCode:"+6",
     name: "",
     email: "",
+    phone: null,
     cuisineSelect: null,
     typeSelect: null,
+    deliveryFee: null,
+    minAmountForDelivery: null,
     orderTypeSelect: null,
-    cuisine: ["Item 1", "Item 2", "Item 3", "Item 4"],
-    type: ["Item 1", "Item 2", "Item 3", "Item 4"],
-    orderType: ["Item 1", "Item 2", "Item 3", "Item 4"],
+    maxDeliveryTimeSelect: null,
+    minDeliveryTimeSelect: null,
+    collectionTimeSelect: null,
+    cuisine: [
+      "Afghan'",
+      "African",
+      "American",
+      "Arabic",
+      "Bagels",
+      "Balti",
+      "Bangladeshi",
+      "BBQ",
+      "Breakfast",
+      "British",
+      "Burgers",
+      "Cakes",
+      "Caribbean",
+      "Chicken",
+      "Chinese",
+      "Curry",
+      "Desserts",
+      "Drinks",
+      "Fish & Chips",
+      "Fusion",
+      "Gourmet",
+      "Gourmet Burgers",
+      "Grill",
+      "Ice Cream",
+      "Indian",
+      "Indonesian",
+      "Iranian",
+      "Italian",
+      "Jamaican",
+      "Japanese",
+      "Kebab",
+      "Korean",
+      "Kosher",
+      "Lebanese",
+      "Lunch",
+      "Mediterranean",
+      "Middle Eastern",
+      "Milkshakes",
+      "Nigerian",
+      "Noodles",
+      "Oriental",
+      "Pakistani",
+      "Peri Peri",
+      "Persian",
+      "Pizza",
+      "Portuguese",
+      "South Indian",
+      "Steak",
+      "Sushi",
+      "Thai",
+      "Turkish",
+      "Vegan",
+      "Vegetarian",
+      "Vietnamese"
+    ],
+    type: ["Vegeterian", "Non-Vegeterian", "Both"],
+    orderType: ["Collection", "Delivery", "Both"],
+    collectionTime: [
+      "5 Minutes",
+      "10 Minutes",
+      "15 Minutes",
+      "20 Minutes",
+      "25 Minutes",
+      "30 Minutes",
+      "35 Minutes",
+      "40 Minutes",
+      "45 Minutes",
+      "50 Minutes",
+      "55 Minutes",
+      "1 Hour"
+    ],
+    maxDeliveryTime: [
+      "5 Minutes",
+      "10 Minutes",
+      "15 Minutes",
+      "20 Minutes",
+      "25 Minutes",
+      "30 Minutes",
+      "35 Minutes",
+      "40 Minutes",
+      "45 Minutes",
+      "50 Minutes",
+      "55 Minutes",
+      "1 Hour"
+    ],
+    minDeliveryTime: [
+      "5 Minutes",
+      "10 Minutes",
+      "15 Minutes",
+      "20 Minutes",
+      "25 Minutes",
+      "30 Minutes",
+      "35 Minutes",
+      "40 Minutes",
+      "45 Minutes",
+      "50 Minutes",
+      "55 Minutes",
+      "1 Hour"
+    ],
     password: "",
     repeatPassword: "",
     description: ""
@@ -141,16 +319,44 @@ export default {
       !this.$v.cuisineSelect.required && errors.push("Cuisine is required");
       return errors;
     },
+    phoneErrors() {
+      const errors = [];
+      if (!this.$v.phone.$dirty) return errors;
+      !this.$v.phone.required && errors.push("Phone Number is required");
+      return errors;
+    },
+    maxDeliveryTimeSelectErrors() {
+      const errors = [];
+      if (!this.$v.maxDeliveryTimeSelect.$dirty) return errors;
+      !this.$v.maxDeliveryTimeSelect.required &&
+        errors.push("Maximum Delivery Time is required");
+      return errors;
+    },
+    minDeliveryTimeSelectErrors() {
+      const errors = [];
+      if (!this.$v.minDeliveryTimeSelect.$dirty) return errors;
+      !this.$v.minDeliveryTimeSelect.required &&
+        errors.push("Minimum Delivery Time is required");
+      return errors;
+    },
+    collectionTimeSelectErrors() {
+      const errors = [];
+      if (!this.$v.collectionTimeSelect.$dirty) return errors;
+      !this.$v.collectionTimeSelect.required &&
+        errors.push("Collection Time is required");
+      return errors;
+    },
     typeErrors() {
       const errors = [];
       if (!this.$v.typeSelect.$dirty) return errors;
       !this.$v.typeSelect.required && errors.push("Type is required");
       return errors;
     },
-        orderTypeSelectErrors() {
+    orderTypeSelectErrors() {
       const errors = [];
       if (!this.$v.orderTypeSelect.$dirty) return errors;
-      !this.$v.orderTypeSelect.required && errors.push("Order Type is required");
+      !this.$v.orderTypeSelect.required &&
+        errors.push("Order Type is required");
       return errors;
     },
     nameErrors() {
@@ -159,6 +365,19 @@ export default {
       !this.$v.name.maxLength &&
         errors.push("Name must be at most 10 characters long");
       !this.$v.name.required && errors.push("Name is required.");
+      return errors;
+    },
+    deliveryFeeErrors() {
+      const errors = [];
+      if (!this.$v.deliveryFee.$dirty) return errors;
+      !this.$v.deliveryFee.required && errors.push("Delivery Fee is required.");
+      return errors;
+    },
+    minAmountForDeliveryErrors() {
+      const errors = [];
+      if (!this.$v.minAmountForDelivery.$dirty) return errors;
+      !this.$v.minAmountForDelivery.required &&
+        errors.push("Minimum Amount for Delivery is required.");
       return errors;
     },
     emailErrors() {
@@ -194,12 +413,16 @@ export default {
     clear() {
       this.$v.$reset();
       this.name = "";
+      this.phone = null;
       this.email = "";
       this.password = "";
       this.repeatPassword = "";
       this.cuisineSelect = null;
       this.typeSelect = null;
       this.orderTypeSelect = null;
+      this.collectionTimeSelect = null;
+      this.maxDeliveryTimeSelect = null;
+      this.minDeliveryTimeSelect = null;
       this.description = "";
       this.checkbox = false;
     }
